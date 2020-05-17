bd6d02ac5ec615755aa986c94f57e83bd4086dbb
gitDGM <64165085+gitDGM@users.noreply.github.com>
2020-05-17 22:27:04 +0200

    Update

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,12 +1,3 @@
 window.onload = function () {
-    var botonMenu = document.querySelector('#pagina #cabecera #botonMenuLateral');
-    var botonQuitMenu = document.querySelector('#pagina #menuDesplegable #quitMenu');
-    var menuDesplegable = document.querySelector('#pagina #menuDesplegable');
-
-    botonMenu.onclick = function() {
-        menuDesplegable.classList.toggle("animateMenu");
-    }
-    botonQuitMenu.onclick = function() {
-        menuDesplegable.classList.toggle("animateMenu");
-    }
+ 
 }
