26c2a4386ba922627fdabdec08847e17f9917695
gitDGM <64165085+gitDGM@users.noreply.github.com>
2020-06-01 23:20:53 +0200

    Update

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,24 +5,17 @@ window.onload = function () {
     
     for(let i = 0;i < skillsInfo.length;i++) {
         skillsInfo[i].onclick = function() {
-            console.log(skillsInfo[i]);
             toggleSkillInfo(this);
         }
     }
 
     function toggleSkillInfo(skillClicked) {
         let infoSkill = skillClicked.children[1];
-        let estado = infoSkill.children[4];
-        console.log(estado);
+        let estado = infoSkill.children[0];
         if (estado.value == 0) {
             estado.value = 1;
             infoSkill.style.maxHeight = "500px";            
             infoSkill.style.padding = "10px 15px";
-            /*
-            let alturaMax = infoSkill.offsetHeight;
-            infoSkill.style.height = "0px";  
-            infoSkill.style.height = alturaMax + 'px';   */
-
         } else {
             estado.value = 0;
             infoSkill.style.maxHeight = "0px";       
