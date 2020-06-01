9e5fa3df6503c5947335af663c28f7b448459208
gitDGM <64165085+gitDGM@users.noreply.github.com>
2020-06-01 21:09:49 +0200

    Update

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,33 @@
 window.onload = function () {
- 
+
+    let skillsInfo = document.querySelectorAll('#pagina main #skills .contenido_seccion .skill');
+
+    
+    for(let i = 0;i < skillsInfo.length;i++) {
+        skillsInfo[i].onclick = function() {
+            console.log(skillsInfo[i]);
+            toggleSkillInfo(this);
+        }
+    }
+
+    function toggleSkillInfo(skillClicked) {
+        let infoSkill = skillClicked.children[1];
+        let estado = infoSkill.children[4];
+        console.log(estado);
+        if (estado.value == 0) {
+            estado.value = 1;
+            infoSkill.style.maxHeight = "250px";            
+            infoSkill.style.padding = "10px 15px";
+            /*
+            let alturaMax = infoSkill.offsetHeight;
+            infoSkill.style.height = "0px";  
+            infoSkill.style.height = alturaMax + 'px';   */
+
+        } else {
+            estado.value = 0;
+            infoSkill.style.maxHeight = "0px";       
+            infoSkill.style.padding = "0px 15px";
+        }
+    }
+
 }
